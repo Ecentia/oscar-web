5c46eb91d49959a45bfa586365b9607edd2d5a45
juanjo210106 <jgamlop2101@g.educaand.es>
2026-01-29 17:47:14 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// No importamos index.css aquí porque todos los estilos críticos están en App.jsx
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
